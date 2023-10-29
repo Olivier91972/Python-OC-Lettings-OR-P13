1bf11219d7954c1cf451ec417d8bf79ba31d6e7f
Olivier Remy <olivier.remy.paris@gmail.com>
2023-10-29 11:11:48 +0100

    Update settings.py - use_tz false

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -96,15 +96,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'fr'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'CET'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
